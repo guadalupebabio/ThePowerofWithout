bd409c04492be10d878f146a654d677baec709df
Jeffrey Shen <jjshen@mit.edu>
2020-05-31 11:22:59 -0700

    set up routes

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-let express = require("express");
-let bodyParser = require("body-parser");
-let mongoose = require("mongoose");
+let express = require("express"),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+
+let app = express(),
+    router = express.Router();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+// ** ROUTES **
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+// ** START THE SERVER **
+app.listen(PORT);
+console.log("Running on http://127.0.0.1:" + PORT);
+module.exports = app;
