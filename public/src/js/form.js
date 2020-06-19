a0219f1ddf35933284a944687882c3e26dd5c129
Jeffrey Shen <jjshen@mit.edu>
2020-06-19 10:41:14 -0700

    select point using form

--- public/src/js/form.js
+++ public/src/js/form.js
@@ -1,7 +1,11 @@
 var map = L.map('map')
   .setView([0,0],3);
 
-L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=AlLtvbLyE9OnbQNyl7PF', {attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'}).addTo(map);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(map);
 
 function goToSection(i){
   $(".column:first-child a").removeClass("active");
@@ -12,3 +16,17 @@ function goToSection(i){
   $("form section:nth-of-type(" + i + ")").removeClass("is-hidden");
   $(".column:nth-child(3) section:nth-of-type(" + i + ")").removeClass("is-hidden");
 }
+
+var marker = null;
+
+map.on('click',function(e){
+   lat = e.latlng.lat;
+   lon = e.latlng.lng;
+
+   console.log(marker == null);
+
+   if(marker == null) marker = L.marker([lat,lon]).addTo(map);
+   else marker.setLatLng(e.latlng);
+
+   $("#coords").val(`${lat.toFixed(5)}, ${lon.toFixed(5)}`)
+});
