9a6e7b3d04f70a3ae18dca2da215e7476adbbac8
Cherie <cheriemmwang@gmail.com>
2020-07-22 15:29:44 -0400

    map zoom in levels and clustermarkers

--- public/src/js/map.js
+++ public/src/js/map.js
@@ -2,7 +2,6 @@ var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
-
 console.log(pointsByCountry);
 
 var countries = $.getJSON('/countries.json', function(data){});
@@ -18,28 +17,77 @@ var token = 'pk.eyJ1IjoiY2hlcmllbW13YW5nIiwiYSI6ImNrYnIwZmkzbzJydm4yb214YW5yMThh
 console.log(typeof points); // Notice: this is an object!
 
 //defining layergroups for data inputs
+var geojson;
 var settlements = L.featureGroup();
 var ch = L.layerGroup();
-
+var selection;
+var selectedLayer;
+var maplayer;
 var populationlist = [];
+var fullcircles = [];
+var overlaysObj = {};
+var countrieslist = [];
+var continentslist = [];
+var markers = L.markerClusterGroup();
+//var markers = L.markerClusterGroup();
+
 
 var populationCountry = 0;
+
+var variables = [
+  'Countries',
+  'Settlements',
+  'Settlement Data'];
+var ranges = {};
+var $select = $('<select></select>')
+  .appendTo($('#variables'))
+  .on('change', function(){
+    setVariable($(this).val());
+  });
+
+for (var i = 0; i < variables.length; i++) {
+  ranges[variables[i]] = { min: Infinity, max: -Infinity };
+  // Simultaneously, build the UI for selecting different
+  // ranges
+  $('<option></option>')
+    .text(variables[i])
+    .attr('value', variables[i])
+    .appendTo($select);
+}
 //add country name and population 
+function checkCountries(country){
+  for (var i = 0; i < countrieslist.length; ++i){
+    if (countrieslist[i] === country){
+      return true;
+    }
+  }
+  countrieslist.push(country);
+  return false; 
+}
 
-    //pointsbycountry
+function checkContinents(continents){
+  for (var i = 0; i < continentslist.length; ++i){
+    if (continentslist[i] === continents){
+      return true;
+    }
+  }
+  continentslist.push(continents);
+  console.log(continentslist);
+  return false; 
+  
+}
 
-    for (var i = 0; i < Object.keys(pointsByCountry).length; ++i){
-      for (var j = 0; j < pointsByCountry[Object.keys(pointsByCountry)[i]].length; ++j) {
-        var pop = pointsByCountry[Object.keys(pointsByCountry)[i]][j]['site']['origin'].population;
-        if (pop == null){
-          pop = 1;
-        }
-        populationCountry +=pop;
+
+for (var i = 0; i < Object.keys(pointsByCountry).length; ++i){
+  for (var j = 0; j < pointsByCountry[Object.keys(pointsByCountry)[i]].length; ++j) {
+    var pop = pointsByCountry[Object.keys(pointsByCountry)[i]][j]['site']['origin'].population;
+      if (pop == null){
+        pop = 1;
+      }
+       populationCountry +=pop;
       }
       
-      //var cou = getObjects(countries.responseJSON, 'name', Object.keys(pointsByCountry)[i]);
       var everypop = {};
-     //console.log(cou);
       everypop.name = Object.keys(pointsByCountry)[i];
       
       everypop.population = populationCountry;
@@ -50,46 +98,79 @@ var populationCountry = 0;
 
     }
 
-//inputting data
-for (var i = 0; i < points.length; ++i){
-  var name = points[i]["name"];
-  var population = points[i]["site"].origin["population"];
-  var causes = points[i]["site"].origin["causes"];
-  var country = points[i]["country"];
-  var continent = points[i]["site"].origin["geolocation"];
+function circlechoose (zoomlevel){
+  
+  for (var i = 0; i < points.length; ++i){
+    var continentcheck;
+    var name = points[i]["name"];
+    var population = points[i]["site"].origin["population"];
+    var causes = points[i]["site"].origin["causes"];
+    var country = points[i]["country"];
+    var continent = points[i]["site"].origin["geolocation"];
+    //var distance = getDistance([lat1, lng1], [lat2, lng2])
+    
+    if (population == null){
+      var rad = 90000;
+    }
+    else{
+      var rad = points[i]["site"].origin["population"]/20;
+    }
 
-  if (population == null){
-    var rad = 90000;
+    //circle colours and size
+    if (causes.localeCompare("Squatting")==0){
+      var col = '#FC4E2A';
+    }
+    else if (causes.localeCompare("Refugee Camp")==0){
+      var col = 'blue';
+    }
+    else if (causes.localeCompare("Illegal Subdivision")==0){
+      var col = 'green';
+    }
+    else{
+      var col = 'orange';
+    }
+  
+    //function createcircle(){
+      var circle = L.circle(points[i]["geolocation"]["coordinates"], {
+        color: col,
+        fillColor: col,
+        fillOpacity: 0.5,
+        radius: 1000
+        });
+      circle.bindPopup(name + "<br>" + country + "<br>" + population + "<br>" + causes + "<br>" + continent);
+      console.log("oaeifuh");
+      //return circle; 
+    //}
+    
+       markers.addLayer(circle);
+  /*   if (zoomlevel == 5){
+      continentcheck = checkContinents(continent);
+      console.log(continentcheck);
+      if (continentcheck === false){
+        var circle = createcircle();
+        settlements.addLayer(circle);
+        //eachcircle(circle);
+      }
+      //settlements.addLayer();
+    }
+    if (zoomlevel == 6){
+      countrycheck = checkCountries(country);
+      console.log(countrycheck);
+      if (countrycheck === false){
+        settlements.removeLayer();
+        console.log("hi");
+        var circle1 = createcircle();
+        console.log(circle1);
+        settlements.addLayer(circle1);
+      }
+    }
+    if (zoomlevel > 6){
+      var circle2 = createcircle();
+        settlements.addLayer(circle2);
+        console.log(settlements);
+    } */
   }
-  else{
-    var rad = points[i]["site"].origin["population"]/20;
-  }
-
-  //circle colours and size
-  if (causes.localeCompare("Squatting")==0){
-    var col = '#FC4E2A';
-  }
-  else if (causes.localeCompare("Refugee Camp")==0){
-    var col = 'blue';
-  }
-  else if (causes.localeCompare("Illegal Subdivision")==0){
-    var col = 'green';
-  }
-  else{
-    var col = 'orange';
-  }
-
-  var circle = L.circle(points[i]["geolocation"]["coordinates"], {
-  color: col,
-  fillColor: col,
-  fillOpacity: 0.5,
-  radius: rad
-  });
-  circle.bindPopup(name + "<br>" + country + "<br>" + population + "<br>" + causes + "<br>" + continent);
-
-  settlements.addLayer(circle);
-  };
-
+}
   //create the map and its base layers
 
   var light   = L.tileLayer(mbUrl, {id: 'mapbox/light-v10', maxZoom: 18, tileSize: 512, zoomOffset: -1, maxZoom: 18,
@@ -108,14 +189,9 @@ for (var i = 0; i < points.length; ++i){
   });
 
   var baseLayers = {
-    "light": light,
-    "dark": dark,
-    "satellite": satellite
-  };
-
-  var overlays = {
-    "settlements": settlements,
-    "countries": ch
+    "Countries": light,
+    "Settlements": dark,
+    "Details": satellite
   };
 
 
@@ -148,7 +224,7 @@ function getColor(d) {
 }
 
   //legend
-var legend = L.control({position: 'bottomright'});
+var legend = L.control({position: 'bottomleft'});
 
 legend.onAdd = function (map) {
 
@@ -170,8 +246,6 @@ legend.addTo(map);
 
 //zooming
 function style(feature) {
-  //console.log(feature.properties.name);
-  //console.log(typeof(feature.properties.name));
   var objwanted = getObjects(populationlist, 'name', feature.properties.name);
   if (objwanted.length == 0 ){
     var popul = 0;
@@ -179,14 +253,14 @@ function style(feature) {
   else{
     var popul = objwanted[0].population;
   }
-  //console.log(popul);
   return {
     weight: 2,
     opacity: 1,
     color: 'white',
     dashArray: '3',
     fillOpacity: 0.7,
-    fillColor: getColor(popul)
+    fillColor: getColor(popul),
+    clickable: true
   };
 
 }
@@ -208,8 +282,6 @@ function highlightFeature(e) {
   info.update(layer.feature.properties.name);
 }
 
-var geojson;
-
 function resetHighlight(e) {
   geojson.resetStyle(e.target);
   info.update();//the country hover info
@@ -218,6 +290,7 @@ function resetHighlight(e) {
 function zoomToFeature(e) {
   map.fitBounds(e.target.getBounds());
   map.addLayer(dark);
+  selectedLayer = markers;
 }
 
 function onEachFeature(feature, layer) {
@@ -227,18 +300,18 @@ function onEachFeature(feature, layer) {
     click: zoomToFeature
   });
 }
-L.control.layers(baseLayers, overlays).addTo(map);
 
  //takes out settlements when it's at satellite or light.
  //this is for now, will change later
   map.on('baselayerchange', function (e) {
       currentLayerID = e.layer._leaflet_id;
-      console.log(currentLayerID); 
-      if(e.layer._leaflet_id == 297 || e.layer._leaflet_id == 242){
-        map.removeLayer(overlays['settlements']);
+      if(e.layer._leaflet_id == 241){
+        map.removeLayer(overlaysObj['settlements']);
+        map.addLayer(overlaysObj['countries']);
       }
       else{
-        map.addLayer(overlays['settlements']);
+        map.addLayer(overlaysObj['settlements']);
+        map.removeLayer(overlaysObj['countries']);
       }
       });
 
@@ -256,46 +329,102 @@ L.control.layers(baseLayers, overlays).addTo(map);
         return objects;
     }
     //figure out how to tie the country into layers
-    $.ajax({}).done(function(data){
+   $.ajax({}).done(function(data){
+            
             var fh = countries.responseJSON;
-            geojson = L.geoJson(fh, {
+     
+            geojson= L.geoJson(fh, {
+            pointToLayer:function(feature,latlng){
+              return L.circleMarker(latlng, myStyle);
+            },
             style: style,
             onEachFeature: onEachFeature
+            });
+            var allPointsLG = L.layerGroup([geojson]).addTo(map);
+            overlaysObj["countries"] = allPointsLG;
+            overlaysObj["settlements"] = markers;
+
+            var control = L.control.layers(baseLayers, overlaysObj, {
+              collapsed: false
             }).addTo(map);
+
+           
           });
+map.on('zoomend', function() {
+  var zoomlevel = map.getZoom();
+      if (zoomlevel  > 4){
+        circlechoose(zoomlevel);
 
-          //onclick function
-   // settlements.on("click", function(){
+        map.addLayer(markers);
+        //console.log(zoomlevel);
+        //console.log("hi");
+        map.addLayer(overlaysObj["settlements"]);
+        map.removeLayer(light);
+        map.addLayer(dark);
+        if (map.hasLayer(overlaysObj["countries"])){
+         map.removeLayer(overlaysObj["countries"]);
+        } 
+       
+      }
+      if (zoomlevel <= 4){
+        map.removeLayer(markers);
+        map.removeLayer(dark);
+        map.addLayer(light);  
+        map.removeLayer(overlaysObj["settlements"]);
+        map.addLayer(overlaysObj["countries"]);
+      }
+      
 
-    //});
+      // circle cluster based on zoom
+      
+});
+            
 
 
-    $(settlements).click(function() {
-        console.log("clicked");
-        //not yet working:
-        //map.addLayer(light);
-        //map.removeLayer(light);
-        //map.removeLayer(dark);
-     //   zoomToFeature;
-    });
+var legendinfo = L.control({position: 'bottomright'}); 
 
+    legendinfo.onAdd = function (map){
+      var div = L.DomUtil.create('div','info legend');
+      div.id = "legendgraph";
+      labels = ['<Strong>Categories</strong'],
+      categories = ['General', 'Site', 'Architecture', 'Population'];
 
+      /* for (var i = 0; i < categories.length; i++) {
+        div.innerHTML +='test'
+           // '<i style="background:' + getColor(labels[i] + 1) + '"></i> ' +
+            //labels[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      } */
+      return div;
+      };
+      legendinfo.addTo(map);
 
-//- map.removeLayer(overlays['settlements']);
-//- map.on('layerremove', function(event) {
-//-     if(event.layer == ch) {
-//-          $.ajax({}).done(function(data){
-//-           var fh = countries.responseJSON;
-//-           geojson = L.geoJson(fh, {
-//-             style: style,
-//-             onEachFeature: onEachFeature
-//-           }).addTo(map);
-//-           });
-//-     }
-//- });
+anychart.onDocumentReady(function(){
+  var data = [
+    {x: "infrastructure", value: 223553265, exploded: true},
+    {x: "physicalNature", value: 38929319},
+    {x: "geolocation", value: 2932248},
+    {x: "Caises", value: 14674252},
+    {x: "Geolocation", value: 540013},
+    {x: "Population", value: 19107368}
+  ];
 
-//- map.on('layeradd', function(event) {
-//-     if(event.layer == ch) {
-//-        console.log("bad");
-//-     }
-//- });
+  var test = [
+    {value: 50},
+    {value: 20},
+    {value: 80}
+
+  ]
+
+  var chart = anychart.pie();
+
+  chart.title("Demonstration of Input Data");
+
+  chart.data(test);
+
+  chart.container('legendgraph');
+  chart.draw();
+  chart.sort("desc");
+  //form data of different qualities. 
+  //depending on zoom level (country, settlements, continents)
+  //display qualitative data percentage
+});
