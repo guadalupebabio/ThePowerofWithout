ca1b890d4ecb4d9d79c0087ad34bb4b8784abc9d
Jeffrey Shen <jjshen@mit.edu>
2020-08-14 13:06:11 -0700

    add info button

--- public/src/js/form.js
+++ public/src/js/form.js
@@ -1,11 +1,31 @@
-var map = L.map('map')
-  .setView([0,0],3);
+if($("#map").length != 0){ // if map exists, create it
+  var map = L.map('map')
+    .setView([0,0],3);
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 19
-}).addTo(map);
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+  	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  	subdomains: 'abcd',
+  	maxZoom: 19
+  }).addTo(map);
+
+  var marker = null;
+
+  map.on('click',function(e){
+     lat = e.latlng.lat;
+     lon = e.latlng.lng;
+
+     if(marker == null) marker = L.marker([lat,lon]).addTo(map);
+     else marker.setLatLng(e.latlng);
+
+     // Set coordinate field
+     $("#coords").val(`${lat.toFixed(5)}, ${lon.toFixed(5)}`);
+
+     // Prefill country values
+     $.get(`/api/get-country?lat=${lat}&lon=${lon}`, function(data) {
+       $("input[name='country']").val(data);
+     });
+  });
+}
 
 function goToSection(i){
   $("#sidebar a").removeClass("active");
@@ -17,31 +37,25 @@ function goToSection(i){
   $("#right-sidebar section:nth-of-type(" + i + ")").removeClass("is-hidden");
 }
 
-var marker = null;
-
-map.on('click',function(e){
-   lat = e.latlng.lat;
-   lon = e.latlng.lng;
-
-   if(marker == null) marker = L.marker([lat,lon]).addTo(map);
-   else marker.setLatLng(e.latlng);
-
-   // Set coordinate field
-   $("#coords").val(`${lat.toFixed(5)}, ${lon.toFixed(5)}`);
-
-   // Prefill country values
-   $.get(`/api/get-country?lat=${lat}&lon=${lon}`, function(data) {
-     $("input[name='country']").val(data);
-   });
-});
-
-function showInfo(e){
-  let hidden = $(e.parentNode.childNodes[1]).css("display") == "none";
-  $(".dropdown-menu").hide(); // Hide all
-  if(hidden) $(e.parentNode.childNodes[1]).show(); // Show if previously hidden
+function showInfo(info, label){
+  $("#info").show();
+  $("#info p").text(`${label}: ${info}`);
 }
 
 // Add Google translate
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
+
+// Info Button Handler
+$(document).click(function(e){
+  console.log("yooo");
+  $("#info").hide();
+});
+
+$(".info-button").click(function(e){
+  let data = $(this).data();
+  e.stopPropagation();
+  $("#info").show();
+  $("#info p").text(`${data.label}: ${data.info}`);
+})
